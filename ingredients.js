641994a35f3f32f3b94959fa5224ea8cb57e1fba
TLAMHutto <keatonhutto8@gmail.com>
2022-06-07 16:00:26 -0500

    adding

--- ingredients.js
+++ ingredients.js
@@ -1,23 +1,44 @@
 const ingredientList = document.querySelector('.ingList');
 const selectedIngredients = document.querySelector('.selectedIngredients');
 const ingSearch = document.querySelector('.ingSearch');
-// const addBtn = document.querySelector('.addBtn');
-const clearBtn = document.getElementById('#undoBtn');
 
 
 
 fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
-  .then(res => res.json())
-  .then(res => {
-    for (let i = 0; i < res.meals.length; i++) {
-    ingredientList.innerHTML += `<li>${res.meals[i].strIngredient}
-    <button onclick="addBtn()" data-ingredient="${res.meals[i].strIngredient}">Add</button></li>`;
-    addBtn=() => {
-        console.log(res.meals[i].strIngredient);
-    }
-    }
-    }
-    );
+.then(res => res.json())
+.then(res => {
+  res.meals.forEach(meal => {
+    const firstList = document.createElement('li');
+    const secondList = document.createElement('li');
+    firstList.innerText = meal.strIngredient;
+    secondList.innerText = meal.strIngredient;
+    const btnAdd = document.createElement('button');
+    const btnRemove = document.createElement('button');
+    btnRemove.innerText = 'ggg';
+    btnAdd.innerText = "+";
+
+
+    btnAdd.addEventListener('click', () => {
+        const addList = document.createElement('li');
+        addList.innerText = meal.strIngredient;
+        selectedIngredients.appendChild(addList);
+    });
+
+    btnRemove.addEventListener('click', () => {
+        const removeList = document.createElement('li');
+        removeList.innerText = meal.strIngredient;
+        selectedIngredients.removeChild(removeList);
+    });
+
+
+    firstList.append(btnAdd);
+    secondList.append(btnRemove);
+    ingredientList.append(firstList);
+    
+  });
+});
+
+
 window.onload = function() {
     ingSearch.addEventListener('keyup', function(e) {
         let search = e.target.value.toLowerCase();
