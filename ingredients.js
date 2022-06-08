16df4bfc0de534bcb0c7529ffdb14c9cc24117fd
TLAMHutto <keatonhutto8@gmail.com>
2022-06-08 03:36:56 -0500

    add

--- ingredients.js
+++ ingredients.js
@@ -9,34 +9,31 @@ fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
 .then(res => {
   res.meals.forEach(meal => {
     const firstList = document.createElement('li');
-    const secondList = document.createElement('li');
     firstList.innerText = meal.strIngredient;
-    secondList.innerText = meal.strIngredient;
     const btnAdd = document.createElement('button');
-    const btnRemove = document.createElement('button');
-    btnRemove.innerText = 'ggg';
     btnAdd.innerText = "+";
-
-
+    const btnRemove = document.createElement('button');
+    btnRemove.innerText = "-";
+    const addList = document.createElement('li');
     btnAdd.addEventListener('click', () => {
-        const addList = document.createElement('li');
+        
         addList.innerText = meal.strIngredient;
         selectedIngredients.appendChild(addList);
+        selectedIngredients.appendChild(btnRemove);
+        
     });
-
     btnRemove.addEventListener('click', () => {
-        const removeList = document.createElement('li');
-        removeList.innerText = meal.strIngredient;
-        selectedIngredients.removeChild(removeList);
+        selectedIngredients.removeChild(addList);
+        selectedIngredients.removeChild(btnRemove);
     });
-
-
     firstList.append(btnAdd);
-    secondList.append(btnRemove);
     ingredientList.append(firstList);
     
   });
 });
+
+
+
 
 
 window.onload = function() {
